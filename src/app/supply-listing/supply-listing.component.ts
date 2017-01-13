3a91dd60a2af70e09f8fc6a5fb5332e125df5917
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-13 04:09:47 -0200

    Implement map list with markers

--- src/app/supply-listing/supply-listing.component.ts
+++ src/app/supply-listing/supply-listing.component.ts
@@ -10,6 +10,11 @@ export class SupplyListingComponent {
 
   supplies = [];
   service;
+  markers;
+
+  lat: number = -27.1136184;
+  lng: number = -50.8356141;
+  zoom = 14;
 
   constructor( service: SupplyService ) {
     this.service = service;
@@ -17,8 +22,31 @@ export class SupplyListingComponent {
     this.service
       .list()
       .then( supplies => this.supplies = supplies )
-      // .then( supplies => console.log( supplies ) )
+      .then( supplies => {
+        this.markers = supplies.map( item => {
+          if ( item.location ) {
+            return {
+              lat: +item.location.split( ',' )[ 0 ] || null,
+              lng: +item.location.split( ',' )[ 1 ] || null,
+              info: item.name
+            }
+          }
+        })
+      })
       .catch( msg => console.log( msg ) )
+
+      //set current position
+      this.setCurrentPosition();
+  }
+
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.zoom = 14;
+      });
+    }
   }
 
 }
