fa1fe1bff5f9749aa7ff9755dfe4c772b02091ce
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-01-13 12:53:33 -0200

    Show formatted validate date

--- src/app/supply/supply.service.ts
+++ src/app/supply/supply.service.ts
@@ -15,7 +15,16 @@ export class SupplyService {
       let supplies = Object.getOwnPropertyNames( localStorage )
         .filter( item => item.split( '-' )[ 0 ] == 'supply' )
         .sort( ( a: any, b: any ) =>  a.split( '-' )[ 1 ] - b.split( '-' )[ 1 ] )
-        .map( item => JSON.parse( localStorage[ item ] ) );
+        .map( item => {
+          let supply = JSON.parse( localStorage[ item ] );
+          let date = new Date( supply.validate );
+          supply.validate = {
+            jsdate: date,
+            formatted: `${ ( "0" + date.getDate() ).slice( -2 ) }/${ ( "0" + ( date.getMonth() + 1 ) ).slice( -2 ) }/${ date.getFullYear() }`
+          }
+
+          return supply;
+        });
 
       setTimeout( () => resolve( supplies ), 1000 );
 
